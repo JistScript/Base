1e7f66f6ad73cd36d91590f8aeac017c6c940836
joeldeondsouza <jdeondsouza@gmail.com>
2024-03-21 13:11:31 +0100

    Test file rerendred

--- main.ts
+++ main.ts
@@ -1,33 +1,33 @@
 import Parser from "./base/parser.ts";
 import { createGlobalEnv } from "./runTime/environment.ts";
 import { evaluate } from "./runTime/interpreter.ts";
-// repl();
+repl();
 
 // test //
-fire("./tests/test1.txt");
+// fire("./tests/test1.txt");
 
-async function fire(filename: string) {
-  const parser = new Parser();
-  const env = createGlobalEnv();
-  // Run time //
-  const input = await Deno.readTextFile(filename);
-  const program = parser.proTypeAsst(input);
-  const result = evaluate(program, env);
-  // console.log(result);
-}
-
-// async function repl() {
+// async function fire(filename: string) {
 //   const parser = new Parser();
 //   const env = createGlobalEnv();
 // Run time //
-// console.log("\nRepl V1");
-// while (true) {
-//   const input = prompt("> ");
-//   if (!input || input.includes("exist")) {
-//     Deno.exit(1);
-//   }
-//     const program = parser.proTypeAsst(input);
-//     const results = evaluate(program, env);
-//     console.log(results);
-//   }
+// const input = await Deno.readTextFile(filename);
+// const program = parser.proTypeAsst(input);
+// const result = evaluate(program, env);
+// console.log(result);
 // }
+
+async function repl() {
+  const parser = new Parser();
+  const env = createGlobalEnv();
+  // Run time //
+  console.log("\nMark:1");
+  while (true) {
+    const input = prompt("> ");
+    if (!input || input.includes("exist")) {
+      Deno.exit(1);
+    }
+    const program = parser.proTypeAsst(input);
+    const results = evaluate(program, env);
+    console.log(results);
+  }
+}
